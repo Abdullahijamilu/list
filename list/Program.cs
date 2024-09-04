9c5fa4e3fff5a5bab66a9db112825828e3c4f292
Abdullahi Jamilu Ohiare <113562405+Abdullahijamilu@users.noreply.github.com>
2024-09-04 14:04:21 -0700

    Add project files.

---
+++ list/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+Console.WriteLine("Hello, World!");
+//Write a code that will calculate students assign grades to
+//students and then return a record with the complete  data
+List<string> Student = new List<string> { "Godwin", "Grace", "Amos",};
+List<string> Studentrecord = new List<string> { "Maths", "English", "chemistry" };
+String[,] StudentGrades =
+{{ "Godwin[maths 50] [English, 70] [chemistry, 20]" },
+    { "Grace[maths 50] [English, 70] [chemistry, 20]"},
+    { "Amos[maths 50] [English, 70] [chemistry, 20]" }
+};
+
+
+for (int i = 0; i < StudentGrades.GetHashCode(); i++)
+{
+    for (int j = 0; j < StudentGrades.GetHashCode(); j++)
+    {
+        Console.Write(StudentGrades[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+    
+
+    //public class Student
+    //{
+    //    public int Id { get; set; }
+    //    public string FullName { get; set; }
+         
+
+
+
+    //}
+
+    //public class StudentRecord
+    //{
+    //    public int StudentId { get; set; }
+    //    public int EnglishScore { get; set; }
+    //    public int MathsScore { get; set; }
+
+    //}
+
---
+++ new class/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+List<string> carTypes = new List<string> { "camry", "nissan", "engen", "corolla", "prado", "cardillac", "Benz", "Ford" };
+//carTypes.Add("corolla");
+//carTypes.Add("prado");
+//carTypes.Add("cardillac");
+//carTypes.Add("Benz");
+//carTypes.Add("Ford");
+
+var carModel = carTypes.Where(toyota => toyota.Contains ("c")).ToList();
+var car = carTypes.Count();
+
+Console.WriteLine($"this car models are of toyota {carModel}");
+
+FoodInventory akara = new FoodInventory("akara", 50);
+FoodInventory pap = new FoodInventory("pap", 20);
+Console.WriteLine(akara.Quantity);
+public class FoodInventory
+{
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    // this is a constructor that saves variable.
+    public FoodInventory(string name, int quantity)
+    {
+        Name = name;
+        Quantity = quantity;
+    }
+    public void IncreaseQuantity(int amount)
+
+    {
+        Quantity += amount;
+    }
+    public void DecreaseQuantity(int amount)
+    {
+        if (Quantity >= amount)
+        {
+            Quantity -= amount;
+        }
+
+    }
+}
+
